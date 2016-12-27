34953535a8fbfbc35518322ba606b939021f7624
wangfei1234 <1425302858@qq.com>
2016-12-27 18:46:55 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
  
-var serverUrl='http://192.168.199.191:7200';
+var serverUrl='http://hiji.hifete.com:7200';
 //var serverUrl='http://192.168.199.191:7200';120.77.53.20:7200
 
 var serverimgUrl='http://211.149.195.183:8114/'; 
