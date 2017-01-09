84493edcfa788c405d51f0f60c2759a1f87117a9
wangfei1234 <1425302858@qq.com>
2017-01-09 11:18:32 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var serverUrl='http://hiji.hifete.com:7200';
  
 //var serverUrl='http://192.168.199.191:7200';120.77.53.20:7200
 
-var serverimgUrl='http://hiji.hifete.com/hijijs'; 
+var serverimgUrl='http://hiji.hifete.com'; 
 //var serverUrl='http://Aaron.lan:7200';  
 //var serverUrl='http://Aaron.lan:7200';
 var pictarget = '';//远程图片地址
