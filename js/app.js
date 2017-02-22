79bf16e279135f8702edf35a22fab84f2b63e58f
wangfei1234 <1425302858@qq.com>
2017-02-22 15:58:39 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
  
- 
+// var serverUrl='http://hiji.hifete.com.ipv4.sixxs.org/';
 var serverUrl='http://hiji.hifete.com:7200';
 //var serverUrl='http://192.168.199.191:7200'; 
 //var serverUrl='http://Aaron.lan:7200';
 
 
 //var serverimgUrl='http://211.149.195.183:8114/'; 
-var serverimgUrl='http://hiji.hifete.com'; 
+var serverimgUrl='http://hiji.hifete.com:6789'; 
 
 var PAYSERVER='http://hiji.hifete.com:7200/api/alipay/payment';
 
