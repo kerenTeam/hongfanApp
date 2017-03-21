e529f3d94e52fedbfef17b17b0e97f17e35320af
youxdd <youxdd0@gmail.com>
2017-03-21 11:37:11 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
  
-var serverUrl='http://192.168.199.191:7200'; 
+//var serverUrl='http://192.168.199.191:7200'; 
 //var serverUrl='http://192.168.0.128:7200';
-//var serverUrl='http://hiji.hifete.com'; 
+var serverUrl='http://hiji.hifete.com'; 
 //var serverUrl='http://Aaron.lan:7200';
   
 
