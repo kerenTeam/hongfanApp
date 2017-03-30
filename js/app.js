b24521b66c297012ab67988b29968b13034057ec
wangfei1234 <1425302858@qq.com>
2017-03-31 01:48:38 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var serverUrl='http://192.168.0.128:7200';
 //var serverUrl='http://hiji.hifete.com'; 
 //var serverUrl='http://Aaron.lan:7200';
 
-//var serverimgUrl='http://211.149.195.183:8114/'; 
+//var serverimgUrl='http://192.168.0.128::6789'; 
 var serverimgUrl='http://hiji.hifete.com:6789'; 
 var PAYSERVER='http://hiji.hifete.com:7200/api/alipay/payment';
 var WXPAYSERVER='http://hiji.hifete.com:7200/api/wechatpay/unified';
