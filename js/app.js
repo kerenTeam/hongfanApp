8fbdd6c286a8e1973966c302da1e8e21b87ef14f
youxdd <youxdd0@gmail.com>
2017-10-31 16:58:00 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 
 //var serverUrl='http://hiji.hifete.com';
-//var serverUrl='http://abcd.zlzmm.com:7200';
+var serverUrl='http://abcd.zlzmm.com:7200';
 //var serverUrl='http://hijiv2.zlzmm.com';
-var serverUrl='http://192.168.199.191:7200';
+//var serverUrl='http://192.168.199.191:7200';
 
 //var serverimgUrl='http://192.168.0.128:6789';
 var serverimgUrl='http://hiji.hifete.com:6789';
