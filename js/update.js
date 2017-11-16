51876cfedbdd959ea75612d681770f9e34bd152c
youxdd <youxdd0@gmail.com>
2017-11-16 19:08:20 +0800

    no message

--- js/update.js
+++ js/update.js
@@ -1,5 +1,6 @@
 var wgtVer=null;
 var wgtUrl = '';
+var newVer = '';
 function plusReady(){
     // 获取本地应用资源版本号
     plus.runtime.getProperty(plus.runtime.appid,function(inf){
@@ -10,117 +11,121 @@ function plusReady(){
 if(window.plus){
     plusReady();
 }else{
-    document.addEventListener('plusready',plusReady,false); 
+    document.addEventListener('plusready',plusReady,false);
 }
-//ios提示更新
+//ios提示更新(弃用)
 function checkUpdateIos(){
-	var oldtoken = plus.storage.getItem('oldToken'); 
+	var oldtoken = plus.storage.getItem('oldToken');
 	mui.ajax(serverUrl+'/api/index/version',{
-		dataType:'json', 
+		dataType:'json',
 		type:'post',
 		timeout:5000,
 		headers:{"token":oldtoken},
-		success:function(data,type,xhr){ 
+		success:function(data,type,xhr){
 			console.log('检测更新',data);
 			console.log('检测更新'+JSON.stringify(data));
 			wgtUrl= serverUrl + data.data.android_path_url;
 			var newVer = data.data.ios_versionNum;//远程版本号
 			var newUrl = data.data.ios_path_url;//远程地址
-			if(newVer > wgtVer){//升级   
+			if(newVer > wgtVer){//升级
 				mui.confirm('检测到有新版本，是否升级？', '提示',['是','否'], function(e) {
-					if(e.index == 0) { 
+					if(e.index == 0) {
 						plus.runtime.openURL(newUrl);
 					}
 				})
             }
-            
+
 		},
 		error:function(xhr,type,errorThrown){
-			//mui.toast('当前网络不好，请重试'); 
+			//mui.toast('当前网络不好，请重试');
 			console.error('操作响应失败');
 		}
 	});
 }
 
 
-// 检测更新
-function checkUpdate(hide){
+// android检测更新（和ios公用）
+function checkUpdate(hide,ios){
 	!hide && plus.nativeUI.showWaiting('正在检查更新...',{width:'130px',height:'110px'});
-	var oldtoken = plus.storage.getItem('oldToken'); 
+	var oldtoken = plus.storage.getItem('oldToken');
 	mui.ajax(serverUrl+'/api/index/version',{
-		dataType:'json', 
+		dataType:'json',
 		type:'post',
 		timeout:5000,
 		headers:{"token":oldtoken},
-		success:function(data,type,xhr){ 
+		success:function(data,type,xhr){
 			console.log('检测更新'+JSON.stringify(data));
-			wgtUrl= serverUrl + data.data.android_path_url;
-			var newVer=data.data.versionNum;//远程版本号
-			if(newVer > wgtVer){//升级  
+			if(ios){
+				wgtUrl = data.data.ios_path_url;//下载地址 ios
+				newVer = data.data.ios_versionNum;//远程版本号（ios
+			}else{
+				wgtUrl=  data.data.android_path_url;//下载地址 android
+				newVer = data.data.versionNum;//远程版本号（android
+			}
+			if(newVer > wgtVer){//升级
 				console.error('开始下载');
-                downWgt(hide,data.data.android_path_url);  // 下载升级包
+                downWgt(hide,wgtUrl,ios);  // 下载升级包
             }else{
-            	plus.nativeUI.closeWaiting();  
-                !hide && plus.nativeUI.alert("当前版本为最新版本！"); 
+            		plus.nativeUI.closeWaiting();
+                !hide && plus.nativeUI.alert("当前版本为最新版本！");
             }
-            
+
 		},
 		error:function(xhr,type,errorThrown){
-			//mui.toast('当前网络不好，请重试'); 
 			console.error('操作响应失败');
 		}
 	});
 }
 // 下载wgt文件
 
-function downWgt(hide,downurl){ 
+function downWgt(hide,downurl,ios){
 	!hide && plus.nativeUI.showWaiting('正在下载...',{width:'130px',height:'110px'});
     console.log('下载地址'+ propUrl+"/"+downurl);
     plus.downloader.createDownload(propUrl+"/"+downurl, {filename:"_doc/update/"}, function(d,status){
-    	console.log('下载状态',status);
-        if ( status == 200 ) { 
-            console.log("下载wgt成功："+d.filename); 
+    		console.log('下载状态',status);
+        if ( status == 200 ) {
+            console.log("下载wgt成功："+d.filename);
             if(!hide){
-            	plus.nativeUI.confirm("下载完成，是否马上升级！",function(e){
-            		var sure = (e.index == 0) ? "确定" : "取消";
-            		if (sure == '确定') { 
-            			installWgt(d.filename,hide); // 安装wgt包 
-            		}
-            	})
+	            	plus.nativeUI.confirm("下载完成，是否马上升级！",function(e){
+	            		var sure = (e.index == 0) ? "确定" : "取消";
+	            		if (sure == '确定') {
+	            			installWgt(d.filename,hide); // 安装wgt包
+	            		}
+	            	})
             }else{
-            	installWgt(d.filename,hide); // 安装wgt包
+            		installWgt(d.filename,hide,ios); // 安装wgt包
             }
-            
+
         } else {
             console.log("下载wgt失败！");
             !hide && plus.nativeUI.alert("下载文件失败！");
         }
-        plus.nativeUI.closeWaiting(); 
+        plus.nativeUI.closeWaiting();
     }).start();
 }
 // 安装应用资源
-function installWgt(path,hide){ 
+function installWgt(path,hide,ios){
     !hide && plus.nativeUI.showWaiting('正在安装...',{width:'130px',height:'110px'});
     plus.runtime.install(path,{},function(){
         plus.nativeUI.closeWaiting();
         console.log("安装wgt文件成功！");
-        //if (!hide) {
-        	plus.nativeUI.alert("软件更新完成， 重启完成升级！",function(){ 
+      	if (!ios) {
+	        	plus.nativeUI.alert("软件更新完成， 重启完成升级！",function(){
 	            plus.runtime.restart();
 	        });
-        //}
-        
+      	}
+
     },function(e){
         plus.nativeUI.closeWaiting();
-        console.log("安装wgt文件失败["+e.code+"]："+e.message); 
+        console.log("安装wgt文件失败["+e.code+"]："+e.message);
         if(!hide){
-        	if (e.message.indexOf('不匹配')>-1) {  
-        		plus.nativeUI.alert("更新完成， 重启完成升级！",function(){ 
+	        	if (e.message.indexOf('不匹配')>-1) {
+	        		plus.nativeUI.alert("更新完成， 重启完成升级！",function(){
 		            plus.runtime.restart();
 		        });
-        	} else{
-        		plus.nativeUI.alert("安装失败["+e.code+"]："+e.message);
-        	} 
+	        	} else{
+	        		plus.nativeUI.alert("安装失败["+e.code+"]："+e.message);
+	        	}
         }
         //!hide && plus.nativeUI.alert("安装失败["+e.code+"]："+e.message);
     });
