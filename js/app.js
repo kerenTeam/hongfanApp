9701347759a5ef0ce96c48f53196b625312e229f
youxdd <youxdd0@gmail.com>
2017-11-27 17:30:07 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 var serverUrl='http://api.zlzmm.com';
 //var serverUrl='http://hijiv2.zlzmm.com';
 //var serverUrl='http://192.168.199.191:7200';
+//var serverUrl='http://192.168.0.12:7200';
 
 //var serverimgUrl='http://192.168.0.128:6789';
 var serverimgUrl='http://hiji.hifete.com:6789';
