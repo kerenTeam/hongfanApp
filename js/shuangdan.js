4fa1c4e7e030302d5629ec967167e95ee3a8c38f
youxdd <youxdd0@gmail.com>
2018-01-24 18:55:45 +0800

    no message

--- js/shuangdan.js
+++ js/shuangdan.js
@@ -1,26 +1,27 @@
 var nowDay = new Date();
-//圣诞节
-var Sbegin = new Date('2017-12-21T00:00:01');
-var Send = new Date('2017-12-26T23:59:59');
-//元旦节
+//圣诞节 弃用
+//18-01-22 年货节发帖
+var Sbegin = new Date('2018-01-22T00:00:01');
+var Send = new Date('2018-02-11T23:59:59');
+//元旦节 弃用
 var Ybegin = new Date('2017-12-29T00:00:01');
 var Yend = new Date('2018-01-01T23:59:59');
-var isOne = '';//当前是那个节日
+var isOne2 = '';//当前是那个节日
 
 if( nowDay>=Sbegin && nowDay<= Send ){
-	isOne = 'SDjie';
-	mui('.SDonePic')[0].style.display = 'inline-block';
-	mui('#shaungdan0')[0].style.display = 'block';
+	isOne2 = 'SDjie2';
+//	mui('.SDonePic')[0].style.display = 'inline-block';
+	mui('#maskInfo123')[0].style.display = 'block';
 }else if( nowDay>=Ybegin && nowDay<= Yend ){
-	isOne = 'YDjie';
-	mui('.SDonePic')[1].style.display = 'inline-block';
+	isOne2 = 'YDjie2';
+//	mui('.SDonePic')[1].style.display = 'inline-block';
 	mui('#shaungdan0')[0].style.display = 'block';
 }
 
-mui('#shaungdan1')[0].onclick = function(){
+mui('#fenxiangBtn2')[0].onclick = function(){
 	event.stopPropagation();
 	if(plus.storage.getItem('myToken')){
-		if(plus.storage.getItem(isOne)){
+		if(plus.storage.getItem(isOne2)){
 			alert('每个用户只能参加一次该活动！');
 		}else{
 			openview({
@@ -43,3 +44,17 @@ mui('#shaungdan1')[0].onclick = function(){
 		});
 	}
 }
+	//关闭弹窗
+	function openMask(){
+		$('.ruleCt').click(function() {
+			event.stopPropagation()
+		})
+		$('#maskInfo123').click(function() {
+			event.preventDefault()
+			$('#maskInfo123').fadeOut(200);
+		})
+		$('.closeMs23').click(function() {
+			event.stopPropagation();
+			$(this).parent().parent().fadeOut(200);
+		})
+	}
