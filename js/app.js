84ade67b418e8187c97ce46a62a4001119dca0da
youxdd <youxdd0@gmail.com>
2018-01-31 18:50:59 +0800

    no message

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ mui.plusReady(function(){
 })
 
 //var serverUrl='http://hijiv2.zlzmm.com';
-//var serverUrl='http://192.168.1.113:7200';
+//var serverUrl='http://192.168.199.191:7200';
 //var serverUrl='http://192.168.0.8:7200';
 
 //var serverimgUrl='http://192.168.0.128:6789';
